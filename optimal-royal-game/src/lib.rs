05b7ba6d07ade16e594f547f0a2f74e05ceb3f82
Casper Guo <casperg@umich.edu>
2024-07-02 11:45:49 -0400

    Benchmarking code

--- optimal-royal-game/src/lib.rs
+++ optimal-royal-game/src/lib.rs
@@ -1,11 +1,12 @@
-mod bfs;
-mod board;
+pub mod board;
+pub mod build_graph;
 mod consts;
 #[cfg(test)]
 mod tests;
 mod types;
 
-use std::collections::{HashMap, HashSet, VecDeque};
-use types::*;
+use serde_json::to_writer;
+use std::collections::{HashMap, HashSet};
+use types::Graph;
 
 pub fn value_iteration(graph: Graph, min_delta: f64, max_iter: usize) {}
--- optimal-royal-game/src/main.rs
+++ optimal-royal-game/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let graph = optimal_royal_game::build_graph::dfs();
+    // println!("{:?}", graph.get(&3206567470190051856));
+    println!("{:?}", graph.len());
 }
